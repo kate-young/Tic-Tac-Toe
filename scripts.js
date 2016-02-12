fe456fdb2dee7b13bac1f946383706c5616a23f7
kate-young <S1391628@student.mcckc.edu>
2016-02-11 19:00:12 -0800

    Check for win

--- scripts.js
+++ scripts.js
@@ -41,6 +41,34 @@ var Board = function() {
           square.x();
           this.last_play = "x";
        }
+       if(this.check_for_win()) {
+           alert(this.last_play + " Wins!");
+       }
+    }
+    this.compareSquares = function(sqr1, sqr2, sqr3) {
+        return (sqr1.value != null && sqr2.value != null && sqr3.value != null) && (sqr1.value === sqr2.value && sqr1.value === sqr3.value);
+    }
+    this.check_for_win = function() {
+        //horizontal
+        if(this.compareSquares(this.getSquare(0,0),this.getSquare(0,1),this.getSquare(0,2))) 
+            return true;
+        if(this.compareSquares(this.getSquare(1,0),this.getSquare(1,1),this.getSquare(1,2)))
+            return true;
+        if(this.compareSquares(this.getSquare(2,0),this.getSquare(2,1),this.getSquare(2,2)))
+            return true;
+        //diagonal
+        if(this.compareSquares(this.getSquare(0,0),this.getSquare(1,1),this.getSquare(2,2)))
+            return true;
+        if(this.compareSquares(this.getSquare(2,0),this.getSquare(1,1),this.getSquare(0,2)))
+            return true;
+        //vertical 
+        if(this.compareSquares(this.getSquare(0,0),this.getSquare(1,0),this.getSquare(2,0)))
+            return true;
+        if(this.compareSquares(this.getSquare(0,1),this.getSquare(1,1),this.getSquare(2,1)))
+            return true;
+        if(this.compareSquares(this.getSquare(0,2),this.getSquare(1,2),this.getSquare(2,2)))
+            return true;
+        return false;
     }
 }
 
@@ -53,15 +81,13 @@ var Square = function(row, column, btn) {
     this.set = function(val) {
         if(!this.value) {
             this.value = val;
+            this.$button.text(val);
         }
-        this.$button.text(val);
     }
     this.x = function() {
         this.set("X");
     }
     this.o = function() {
         this.set("O");
-    }
-    this.surrounding = function() { 
     }
 }
